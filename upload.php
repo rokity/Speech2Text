0c70ea0677f9412012cfb6fbba48a4962c259bfe
giottocagna <giottocagna@gmail.com>
2015-06-23 20:02:51 +0200

    test

--- upload.php
+++ upload.php
@@ -72,7 +72,7 @@ echo "File flac is ready for to be converted in text </br>";
 
 //CAMBIARE QUANDO STA SUL SERVER
 //$file = realpath( '/home/a2124875/public_html/quick.flac');
-$file= realpath(  $real."ciao.flac");
+$file= $real."ciao.flac";
 $bitRate = 44100; // The bit rate of the file
 $result = $speech->process($file, $bitRate, 'it-IT');
 print_r(error_get_last());
