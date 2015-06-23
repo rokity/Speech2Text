77f5540c81c1e63c8c1a844822b344f9b13a4ebe
giottocagna <giottocagna@gmail.com>
2015-06-23 17:48:38 +0200

    Copy file php

---
+++ upload.php
@@ -0,0 +1,88 @@
+<?php
+echo ini_get("disable_functions");
+//$fname=$_POST['fname'];
+//$fcontent=$_POST['data'];
+
+$data = substr($_POST['data'], strpos($_POST['data'], ",") + 1);
+$decodedData = base64_decode($data);
+$filename = "ciao.mp3";
+$real="/home/a2124875/public_html/dir/";
+$e=null;
+try
+    {
+      
+
+
+echo $real.$filename;
+      $fp = fopen($real.$filename, 'wb');
+
+      if ( !$fp ) {
+        throw new Exception('File open failed.');
+      }
+      
+     fwrite($fp, $decodedData);
+     
+fclose($fp);
+
+      // send success JSON
+
+    } catch ( Exception $e ) {
+        
+     echo "error:</br>";
+     print_r( $e);
+     echo " errrors:";
+     print_r(error_get_last());
+     
+    }
+    
+    
+    if($e==null){
+        
+        echo "</br>File is ready for the convertion in Flac </br>";
+        $real=realpath( __DIR__ );
+        //"/flac2mp3/./mp32flac.sh ";
+        $output = system("ffmpeg -i /home/a2124875/public_html/dir/ciao.mp3 /home/a2124875/public_html/dir/ciao.flac");
+       // echo "</br> E".$output;
+        echo "Convertion is finish </br>";
+        echo  realpath(  '/home/a2124875/public_html/dir/ciao.flac');
+        echo "</br>";
+        print_r(error_get_last());
+
+    
+        
+        include 'GoogleSpeechToText.php';
+
+// Your API Key goes here.
+
+
+$speech = new GoogleSpeechToText("AIzaSyDNsTkS0Jm6NQRFbaHKBHsaRO6BXNcjOtw");
+echo "File flac is ready for to be converted in text </br>";
+
+//CAMBIARE QUANDO STA SUL SERVER
+//$file = realpath( '/home/a2124875/public_html/quick.flac');
+$file= realpath(  '/home/a2124875/public_html/dir/ciao.flac');
+$bitRate = 44100; // The bit rate of the file.
+$result = $speech->process($file, $bitRate, 'it-IT');
+
+if(!empty($result)){
+  echo "</br> The result: </br>";
+$array=$result[0]['alternative'];
+foreach($array as $value){
+    if(!empty($value['confidence']))
+        echo $value['transcript'];
+
+    }
+    }
+    else{
+      echo "Please Retry";
+    }
+
+    echo "</br>";
+    print_r(error_get_last());
+    unlink('/home/a2124875/public_html/dir/ciao.flac');
+    unlink( '/home/a2124875/public_html/dir/ciao.mp3');
+    
+
+    }
+
+?>
